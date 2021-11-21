dcf9d85b44d0a4a57841eb4be5eb6ef46444defe
Alejandro Celaya <alejandro@alejandrocelaya.com>
2021-11-21 13:35:04 +0100

    Created InvalidConfigException test

---
+++ test/Config/Exception/InvalidConfigExceptionTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ShlinkioTest\Shlink\SDK\Config\Exception;
+
+use PHPUnit\Framework\TestCase;
+use Shlinkio\Shlink\SDK\Config\Exception\InvalidConfigException;
+
+class InvalidConfigExceptionTest extends TestCase
+{
+    /** @test */
+    public function createsExceptionForMissingEnvVars(): void
+    {
+        $e = InvalidConfigException::forMissingEnvVars();
+
+        self::assertEquals(
+            'Either "SHLINK_BASE_URL" and/or "SHLINK_API_KEY" env vars are missing. Make sure both are properly set.',
+            $e->getMessage(),
+        );
+    }
+
+    /** @test */
+    public function createsExceptionForInvalidConfig(): void
+    {
+        $e = InvalidConfigException::forInvalidConfig();
+
+        self::assertEquals(
+            'Provided array is missing "baseUrl" and/or "apiKey" props, or their values are invalid. Make sure both '
+            . 'are set with strings.',
+            $e->getMessage(),
+        );
+    }
+}
