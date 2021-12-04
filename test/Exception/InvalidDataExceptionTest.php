d336f0be9f4911627063251a10ed57de4145bc4a
Alejandro Celaya <alejandro@alejandrocelaya.com>
2021-12-04 09:48:36 +0100

    Created InvalidDataExceptionTest

---
+++ test/Exception/InvalidDataExceptionTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ShlinkioTest\Shlink\SDK\Exception;
+
+use PHPUnit\Framework\TestCase;
+use Shlinkio\Shlink\SDK\Exception\InvalidDataException;
+use Shlinkio\Shlink\SDK\Http\Exception\HttpException;
+
+class InvalidDataExceptionTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider provideExceptions
+     */
+    public function exceptionIsBuiltAsExpected(
+        HttpException $prev,
+        string $expectedMessage,
+        int $expectedCode,
+        array $expectedInvalidElements,
+    ): void {
+        $e = InvalidDataException::fromHttpException($prev);
+
+        self::assertEquals($expectedMessage, $e->getMessage());
+        self::assertEquals($expectedCode, $e->getCode());
+        self::assertEquals($expectedInvalidElements, $e->invalidElements());
+    }
+
+    public function provideExceptions(): iterable
+    {
+        yield 'no invalidElements' => [
+            HttpException::fromPayload(['detail' => $message = 'This is the message']),
+            $message,
+            -1,
+            [],
+        ];
+        yield 'invalidElements' => [
+            HttpException::fromPayload(
+                ['detail' => $message = 'Foobar', 'invalidElements' => $invalidElements = ['foo', 'bar']],
+            ),
+            $message,
+            -1,
+            $invalidElements,
+        ];
+        yield 'custom status' => [
+            HttpException::fromPayload(['status' => $status = 404]),
+            '',
+            $status,
+            [],
+        ];
+    }
+}
